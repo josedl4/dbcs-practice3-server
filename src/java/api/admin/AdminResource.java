1da1a1ce05c2018c40d118da963d958113951ec1
Jose L Martin Martin <joseop21@gmail.com>
2017-12-15 21:58:58 +0100

    Completada funcionalidad del administrador

--- src/java/api/admin/AdminResource.java
+++ src/java/api/admin/AdminResource.java
@@ -3,10 +3,10 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package api.login;
+package api.admin;
 
-import Dominio.Usuario;
-import Persistencia.UsuarioFacadeLocal;
+import Dominio.Jugador;
+import Persistencia.JugadorFacadeLocal;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.naming.InitialContext;
@@ -20,52 +20,61 @@ import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
-import javax.ws.rs.core.Application;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
-@Path("login")
-public class LoginResource extends Application {
-    UsuarioFacadeLocal usuarioFacade = lookupUsuarioFacadeLocal();
+/**
+ * REST Web Service
+ *
+ * @author joselm
+ */
+@Path("admin")
+public class AdminResource {
+    JugadorFacadeLocal jugadorFacade = lookupJugadorFacadeLocal();
 
     @Context
     private UriInfo context;
 
     /**
-     * Creates a new instance of LoginResource
+     * Creates a new instance of AdminResource
      */
-    public LoginResource() {
+    public AdminResource() {
     }
-    
+
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public Response login(String content) throws ParseException {
+    public Response importPlayer(String content) throws ParseException {
+        
         JSONParser parser = new JSONParser();
         JSONObject json = (JSONObject) parser.parse(content);
+        Jugador jugador = new Jugador();
+        jugador.setContractuntil((String) json.get("contractuntil"));
+        jugador.setDateofbirth((String) json.get("dateofbirth"));
+        jugador.setJerseynumber(Math.toIntExact((long)json.get("jerseynumber")));
+        jugador.setMarketvalue((String) json.get("marketvalue"));
+        jugador.setName((String) json.get("name"));
+        jugador.setNationality((String) json.get("nationality"));
+        jugador.setPosition((String) json.get("position"));
         
-        Usuario us = usuarioFacade.find((String)json.get("username"));
-        if(us == null){
-            return Response.status(400).build();
-        } else if(us.getClave().equals((String)json.get("password"))){
-            return Response.status(200).build();
-        } else {
-            return Response.status(402).build();
-        }
+        jugadorFacade.create(jugador);
+        
+        if(!jugadorFacade.findAll().contains(jugador))
+            return Response.status(500).build();
+        
+        return Response.status(200).build();
     }
 
-    private UsuarioFacadeLocal lookupUsuarioFacadeLocal() {
+    private JugadorFacadeLocal lookupJugadorFacadeLocal() {
         try {
             javax.naming.Context c = new InitialContext();
-            return (UsuarioFacadeLocal) c.lookup("java:global/dbcs-practice3-server/UsuarioFacade!Persistencia.UsuarioFacadeLocal");
+            return (JugadorFacadeLocal) c.lookup("java:global/dbcs-practice3-server/JugadorFacade!Persistencia.JugadorFacadeLocal");
         } catch (NamingException ne) {
             Logger.getLogger(getClass().getName()).log(Level.SEVERE, "exception caught", ne);
             throw new RuntimeException(ne);
         }
     }
-    
-    
 }
