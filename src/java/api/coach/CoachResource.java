9434d12eeb601e7c5fa9dcd5a506c4c8d25411b2
Jose L Martin Martin <joseop21@gmail.com>
2017-12-20 17:47:27 +0100

    Ultimos cambios

--- src/java/api/coach/CoachResource.java
+++ src/java/api/coach/CoachResource.java
@@ -16,6 +16,7 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
 import javax.ws.rs.core.Response;
@@ -74,7 +75,7 @@ public class CoachResource {
      * @param player int que identifica al jugador
      * @return Response con codigo de exito si se hizo correctamente
      */
-    @PUT
+    @POST
     @Produces("application/json")
     @Path("/{coach}/jugador/{player}")
     public Response addJugadorEquipo(@PathParam("coach") String coach,
